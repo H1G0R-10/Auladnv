e0ba17a2b9db1fbfc512d6fa01b034857dc11aa3
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-17 17:10:11 -0300

    blz

--- React/React/src/App.jsx
+++ React/React/src/App.jsx
@@ -1,35 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export default function App(){
+  const [name, setName] = useState("")
+  const [theme, setTheme] = useState(true)
   const [count, setCount] = useState(0)
+  const [titulo, setTitulo] = useState(true)
+  const [senai, setSenai] = useState("") 
+  
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        <div className="card">
+        <button onClick={() => setCount((count) => count + 10)}>Adicionar {count}</button>
+        </div>
+        <div className="card">
+        <button onClick={() => setCount((count) => count - 10)}>Remover {count}</button>
+        </div>
+
+        <button onClick={() => setTitulo(titulo ? false : true)}>botao</button>
+        {titulo ?
+
+        <div>
+          <h1>Balacobaco</h1>
+        </div>
+
+        : <div></div>
+        }
+        <button onClick={() => setSenai(senai ? false : true)}>ver</button>
+        {senai ?
+
+        <div>
+          <input type="text" onChange={(e) => setSenai(e.target.value = "senai")}/>
+          <h1>Apareci!</h1>
+
+        </div>
+
+        : <div>
+          <input type="text" onChange={(e) => setSenai(e.target.value)}/>
+
+        </div>
+        }
+        
+
+
+
+
+
+
+        {/* <button onClick={() => setTheme(theme ? false : true)}>TROCAR A COR DO FUNDO</button>
+        {theme ?
+
+        <div className='fundo-preto'>
+          <h1>NOME: {name}</h1>
+          <input type="text" onChange={(e) => setName(e.target.value)}/>
+        </div>
+
+        : <div className='fundo-branco'>
+        <h1>NOME: {name}</h1>
+        <input type="text" onChange={(e) => setName(e.target.value)}/>
+        </div>
+        }  */}
+        
+       
     </>
   )
 }
-
-export default App
